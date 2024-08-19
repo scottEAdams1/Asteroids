4f5e0035a290fbbf75bf6e8fca9d0bfaf4bbc5f2
scottEAdams1 <scott.e.adams01@gmail.com>
2024-08-19 23:48:08 +0100

    Added collisions between shots and asteroids, asteroids split into smaller ones when shot

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from constants import *
 from circleshape import CircleShape
 
@@ -11,4 +12,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
         
+        angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(angle)
+        velocity2 = self.velocity.rotate(-angle)
+
+        radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, radius)
+        
+        asteroid1.velocity = velocity1 * 1.2
+        asteroid2.velocity = velocity2 * 1.2
